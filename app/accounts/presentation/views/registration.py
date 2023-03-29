98752168b4946edc49622352911d918065cb32c6
xcixor <pndungu54@gmail.com>
2023-03-29 16:35:41 +0300

    feat(registration): implement registration

--- app/accounts/presentation/views/registration.py
+++ app/accounts/presentation/views/registration.py
@@ -1,11 +1,9 @@
 from django.conf import settings
 from django.contrib.auth import login
 from django.contrib import messages
-from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views import View
 from django.views.generic import TemplateView, FormView
-from django_htmx.http import HttpResponseClientRedirect
 from accounts.forms import RegistrationForm
 
 
@@ -15,7 +13,7 @@ class GetRegistrationView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['form'] = RegistrationForm
+        context['form'] = RegistrationForm(self.request)
         return context
 
 
@@ -23,11 +21,9 @@ class PostRegistrationView(FormView):
 
     form_class = RegistrationForm
     template_name = 'accounts/registration/registration.html'
-    partial_template_name = 'accounts/registration/partial/registration.html'
     success_url = '/accounts/dashboard/'
 
     def form_valid(self, form):
-        self.partial_template_name = 'accounts/registration/partial/registration_success.html'
         self.request.session.pop('registration_details', None)
         new_user = form.save()
         if not settings.DEBUG:
@@ -36,14 +32,13 @@ class PostRegistrationView(FormView):
         success_message = _("Registration successful!")
         messages.add_message(
             self.request, messages.SUCCESS, success_message)
-        if self.request.htmx:
-            return HttpResponseClientRedirect(self.success_url)
         return super().form_valid(form)
 
-    def get_template_names(self):
-        if self.request.htmx:
-            return self.partial_template_name
-        return self.template_name
+    def get_form_kwargs(self):
+        """Return the keyword arguments for instantiating the form."""
+        kwargs = super(PostRegistrationView, self).get_form_kwargs()
+        kwargs['request'] = self.request
+        return kwargs
 
     def form_invalid(self, form):
         print(form.errors.as_data())
@@ -54,9 +49,6 @@ class PostRegistrationView(FormView):
         registration_details.pop('password2', None)
         self.request.session['registration_details'] = registration_details
         return super().form_invalid(form)
-
-    # def get_success_url(self) -> str:
-    #     return reverse('accounts:dashboard')
 
 
 class RegistrationView(View):
