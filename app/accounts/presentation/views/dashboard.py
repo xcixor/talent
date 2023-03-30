4702c5d68685e8dfe4fc2312939505b186b3794e
xcixor <pndungu54@gmail.com>
2023-03-30 14:25:29 +0300

    chore(accounts): render different templates depending on the user

--- app/accounts/presentation/views/dashboard.py
+++ app/accounts/presentation/views/dashboard.py
@@ -5,3 +5,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class DashboardView(LoginRequiredMixin, TemplateView):
 
     template_name = 'accounts/dashboard.html'
+    superuser_dashboard = 'infinite_admin/dashboard.html'
+
+    def get_template_names(self):
+        if self.request.user.is_superuser:
+            return self.superuser_dashboard
+        return self.template_name
