f0c3efccc860e7fe86d38f07f715cacb4108b228
xcixor <pndungu54@gmail.com>
2023-03-30 21:28:09 +0300

    feat(profile): update personal info

--- app/accounts/forms/__init__.py
+++ app/accounts/forms/__init__.py
@@ -1,3 +1,4 @@
 from .registration_form import JobSeekerRegistrationForm
 from .login_form import LoginForm
 from .email_change import EmailChangeForm
+from .update_personal_info import UpdatePersonalInfoForm
