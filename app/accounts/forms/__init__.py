753d0f9154951bf8e17e41139971264ae363b2c1
xcixor <pndungu54@gmail.com>
2023-03-30 17:02:26 +0300

    chore(forms): rename job seeker registration form appropriately

--- app/accounts/forms/__init__.py
+++ app/accounts/forms/__init__.py
@@ -92,7 +92,7 @@ def get_countries(placeholder):
     return countries
 
 
-class RegistrationForm(forms.ModelForm, HtmlEmailMixin):
+class JobSeekerRegistrationForm(forms.ModelForm, HtmlEmailMixin):
 
     password1 = forms.CharField(widget=forms.PasswordInput)
     password2 = forms.CharField(widget=forms.PasswordInput)
@@ -173,7 +173,7 @@ class RegistrationForm(forms.ModelForm, HtmlEmailMixin):
             'type_of_visa', 'resume', 'linkedin_url', 'mode_of_contact']
 
     def __init__(self, request, *args, **kwargs):
-        super(RegistrationForm, self).__init__(*args, **kwargs)
+        super(JobSeekerRegistrationForm, self).__init__(*args, **kwargs)
         data = request.session.pop('registration_details', None)
         if data:
             self.fields['preferred_country'].choice = data['preferred_country']
@@ -256,87 +256,13 @@ class RegistrationForm(forms.ModelForm, HtmlEmailMixin):
             context=context)
 
     def save(self, commit=True):
-        user = super(RegistrationForm, self).save(commit=False)
+        user = super(JobSeekerRegistrationForm, self).save(commit=False)
         user.set_password(self.cleaned_data["password1"])
         user.is_active = True
+        user.type_of_user = 'JOB_SEEKER'
         if commit:
             user.save()
             # self.notify_admin(user)
-        return user
-
-
-class ResendActivationEmail(forms.Form, HtmlEmailMixin):
-
-    email = forms.EmailField(required=True)
-
-    def get_account(self, email):
-        existing_user = None
-        try:
-            existing_user = User.objects.get(email=email)
-        except User.DoesNotExist as de:
-            print(de)
-        return existing_user
-
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-        existing_user = self.get_account(email)
-        if not existing_user:
-            raise forms.ValidationError(
-                _("Sorry that account does not exist."), code='invalid')
-        if existing_user.is_active:
-            raise forms.ValidationError(
-                _(
-                    'Your account is already active. '
-                    'Please login with the details you registered with, '
-                    'or reset your password'), code='invalid')
-        return email
-
-    def resend_account_activation_email(self, request):
-        email = self.cleaned_data.get("email")
-        user = self.get_account(email)
-        if not user:
-            raise forms.ValidationError(
-                _("Sorry the account does not exist."), code='invalid')
-        to_email = user.email
-        subject = _("Welcome to Agripitch 2022!")
-        from_email = settings.VERIFIED_EMAIL_USER
-        current_site = get_current_site(request)
-        context = {
-            "username": user.username,
-            "email": user.email,
-            'domain': current_site.domain,
-            "protocol": request.scheme,
-            'email_head': subject,
-            "uid": urlsafe_base64_encode(force_bytes(user.pk)),
-            'token': account_activation_token.make_token(user)
-        }
-        return super().send_email(
-            subject, None, from_email, [to_email],
-            template='registration/email/account_activation.html',
-            context=context)
-
-    def notify_admin(self, user):
-        subject = _(
-            'New User Registration')
-        from_email = settings.VERIFIED_EMAIL_USER
-        to_email = settings.ADMIN_EMAILS
-        context = {
-            'email_address': user.email,
-            'time': timezone.now()
-        }
-        return super().send_email(
-            subject, None, from_email, to_email,
-            template='registration/email/admin/new_user.html',
-            context=context)
-
-    def save(self, commit=True):
-        user = super(RegistrationForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.is_active = True
-            user.save()
-        # if not settings.DEBUG:
-        #     self.notify_admin(user)
         return user
 
 
