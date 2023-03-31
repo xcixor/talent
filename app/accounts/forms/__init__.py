83fc45df207af1404151904242ce0ecddfb52075
xcixor <pndungu54@gmail.com>
2023-03-31 14:35:42 +0300

    feat(profile): update resume

--- app/accounts/forms/__init__.py
+++ app/accounts/forms/__init__.py
@@ -3,3 +3,4 @@ from .login_form import LoginForm
 from .email_change import EmailChangeForm
 from .update_personal_info import UpdatePersonalInfoForm
 from .update_basic_info import UpdateBasicInfoForm
+from .update_resume import UpdateResumeForm
