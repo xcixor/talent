5aa54a13b6ca036b19e44342f2904b91c511814b
xcixor <pndungu54@gmail.com>
2023-03-31 15:52:26 +0300

    fix(registration): ensure materialize css datepicker is initialized correctly

--- app/accounts/static/js/accounts/registration/registration.js
+++ app/accounts/static/js/accounts/registration/registration.js
@@ -37,16 +37,6 @@ prevBtn.forEach((button) => {
 	});
 });
 
-// form.addEventListener("submit", (e) => {
-// 	e.preventDefault();
-// 	const inputs = [];
-// 	form.querySelectorAll("input").forEach((input) => {
-// 		const { name, value } = input;
-// 		inputs.push({ name, value });
-// 	});
-// 	console.log(inputs);
-// 	form.reset();
-// });
 
 function isEmptyOrSpaces(str) {
 	return str === null || str.match(/^ *$/) !== null;
