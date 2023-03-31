34432d80bec8fc04d8a9188561bbbf666560958d
xcixor <pndungu54@gmail.com>
2023-03-31 15:26:42 +0300

    style(messages): enable the closing of messages

--- app/common/static/js/common/base.js
+++ app/common/static/js/common/base.js
@@ -1,5 +1,17 @@
 $(".dropdown-trigger").dropdown();
 $(document).ready(function () {
+	setTimeout(function(){
+        $("#messagesContainer").fadeOut( "slow" );
+     }, 10000);
 	$(".sidenav").sidenav();
+});
+
+var closeMessageContainer = function(){
+    $("#messagesContainer").fadeOut( "fast" );
+}
+
+$("#closeMessagesIcon").on("click", ()=>{
+	console.log("closing")
+	closeMessageContainer();
 });
 
