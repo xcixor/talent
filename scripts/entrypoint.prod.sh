8dc4e76080d2b7ee09b94544c61c2dce4034aa41
xcixor <pndungu54@gmail.com>
2023-04-08 11:01:05 +0300

    chore(commands): create admin dynamically using django commands

--- scripts/entrypoint.prod.sh
+++ scripts/entrypoint.prod.sh
@@ -2,5 +2,6 @@
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
+python manage.py create_admin
 gunicorn -b 0.0.0.0:8500 core.wsgi:application --timeout 90
 exec "$@"
