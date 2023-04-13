b88bb1cfd6d0a366bfa560d10d70a894fcbe09fa
xcixor <pndungu54@gmail.com>
2023-04-13 19:49:35 +0300

    chore(db): add model to track job categories

--- app/job/models/job.py
+++ app/job/models/job.py
@@ -2,8 +2,25 @@ from django.db import models
 from accounts.models import User
 
 
+class JobCategory(models.Model):
+
+    title = models.CharField(
+        max_length=255)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+
+        verbose_name_plural = 'Job Categories'
+
+
 class JobListing(models.Model):
 
+    category = models.ForeignKey(
+        JobCategory,
+        on_delete=models.CASCADE, related_name='openings')
     job_owner = models.ForeignKey(
         User,
         on_delete=models.CASCADE, related_name='job')
