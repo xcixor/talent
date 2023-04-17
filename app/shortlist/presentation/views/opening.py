959d2df56fa86d0c10ef9118a9b9f558428b6094
xcixor <pndungu54@gmail.com>
2023-04-17 11:50:08 +0300

    feat(similar): display a few similar jobs

--- app/shortlist/presentation/views/opening.py
+++ app/shortlist/presentation/views/opening.py
@@ -7,3 +7,11 @@ class OpeningView(DetailView):
     model = JobListing
     context_object_name = 'opening'
     template_name = 'shortlist/opening.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        opening = self.get_object()
+        similar = JobListing.objects.filter(
+            industry=opening.industry).exclude(pk=opening.pk)[:3]
+        context['similar'] = similar
+        return context
