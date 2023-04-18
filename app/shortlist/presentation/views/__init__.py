9f461c76448889de9ee484629ff35f621faf8032
xcixor <pndungu54@gmail.com>
2023-04-18 11:22:43 +0300

    feat(application): apply to openings

--- app/shortlist/presentation/views/__init__.py
+++ app/shortlist/presentation/views/__init__.py
@@ -2,3 +2,4 @@ from .openings import OpeningsView
 from .clear_filters import ClearFiltersView
 from .opening import OpeningView
 from .industry_openings import IndustryOpeningsView
+from .application import ApplicationView
