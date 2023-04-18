edf5e826f1449092ae863684bad0b86e54e74237
xcixor <pndungu54@gmail.com>
2023-04-18 13:56:49 +0300

    feat(registration): register staff

--- app/accounts/presentation/views/dashboard.py
+++ app/accounts/presentation/views/dashboard.py
@@ -7,10 +7,13 @@ class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'accounts/dashboard.html'
     superuser_dashboard = 'infinite_admin/dashboard.html'
     employer_dashboard = 'accounts/profile/employer/dashboard.html'
+    staff_dashboard = 'accounts/profile/staff/dashboard.html'
 
     def get_template_names(self):
         if self.request.user.is_superuser:
             return self.superuser_dashboard
+        if self.request.user.is_staff:
+            return self.staff_dashboard
         elif self.request.user.type_of_user == 'EMPLOYER':
             return self.employer_dashboard
         return self.template_name
