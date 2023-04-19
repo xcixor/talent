3dace1c373ce69545b4108c88abc927bf2344e7f
xcixor <pndungu54@gmail.com>
2023-04-19 09:56:45 +0300

    fix(models): add correct status properties

--- app/shortlist/models/shortlist.py
+++ app/shortlist/models/shortlist.py
@@ -3,15 +3,6 @@ from accounts.models import User
 from shortlist.models import Application
 
 
-SHORTLIST_STATUS = [
-    ('UNASSIGNED', 'UNASSIGNED'),
-    ('IN_PROGRESS', 'IN_PROGRESS'),
-    ('ACCEPTED', 'ACCEPTED'),
-    ('DECLINED', 'DECLINED'),
-    ('COMPLETED', 'COMPLETED'),
-]
-
-
 class ShortList(models.Model):
 
     application = models.ForeignKey(
@@ -21,9 +12,7 @@ class ShortList(models.Model):
         User, on_delete=models.SET_NULL, null=True,
         related_name='shortlists')
     created = models.DateTimeField(auto_now=True)
-    status = models.CharField(
-        max_length=40, default='UNASSIGNED',
-        choices=SHORTLIST_STATUS)
+    status = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return f"{str(self.id).zfill(3)} In review by {self.shortlister}"
