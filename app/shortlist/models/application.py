2f4db2577c34d051613a5a9cbfd7e01f7428fa18
xcixor <pndungu54@gmail.com>
2023-04-19 09:56:45 +0300

    fix(models): add correct status properties

--- app/shortlist/models/application.py
+++ app/shortlist/models/application.py
@@ -3,6 +3,15 @@ from accounts.models import User
 from job.models import JobListing
 
 
+SHORTLIST_STATUS = [
+    ('UNASSIGNED', 'UNASSIGNED'),
+    ('IN_PROGRESS', 'IN_PROGRESS'),
+    ('ACCEPTED', 'ACCEPTED'),
+    ('DECLINED', 'DECLINED'),
+    ('COMPLETED', 'COMPLETED'),
+]
+
+
 class Application(models.Model):
 
     applicant = models.ForeignKey(
@@ -12,8 +21,10 @@ class Application(models.Model):
         JobListing, on_delete=models.CASCADE,
         related_name='applications')
     created = models.DateTimeField(auto_now=True)
-    status = models.BooleanField(default=False)
     in_review = models.BooleanField(default=False)
+    status = models.CharField(
+        max_length=40, default='UNASSIGNED',
+        choices=SHORTLIST_STATUS)
 
     def __str__(self) -> str:
         return f"Application {str(self.pk).zfill(3)} by {self.applicant}"
