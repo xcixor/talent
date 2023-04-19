f80994b2dc2fee79422347ef8ac0a3d47c7663b5
xcixor <pndungu54@gmail.com>
2023-04-19 11:32:53 +0300

    feat(shortlist): shortlist

--- app/shortlist/presentation/views/staff/shortlist.py
+++ app/shortlist/presentation/views/staff/shortlist.py
@@ -5,6 +5,7 @@ from django.utils.translation import gettext_lazy as _
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from shortlist.models import ShortList
+from shortlist.forms import UpdateShortListForm
 
 
 class ShortListView(PermissionRequiredMixin, DetailView):
@@ -15,6 +16,11 @@ class ShortListView(PermissionRequiredMixin, DetailView):
     permission_denied_message = _(
         'Hmm it seems that you cannot review this application '
         'please contact your admin.')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({'form': UpdateShortListForm(self.get_object())})
+        return context
 
     def handle_no_permission(self):
         messages.add_message(
