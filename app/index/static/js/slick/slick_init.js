f5d02c39787ce7bb80d96b454efec25b1ca74ecd
xcixor <pndungu54@gmail.com>
2023-04-30 15:51:28 +0300

    chore(index): add more sections in index page

--- app/index/static/js/slick/slick_init.js
+++ app/index/static/js/slick/slick_init.js
@@ -10,12 +10,79 @@ $(document).ready(function(){
                 breakpoint: 600,
                 settings: {
                     arrows: false,
-                    dots:true
+                    dots:true,
+                    adaptiveHeight: true
                 }
                 }
         ]
     });
     $('.background-image').css({'visibility':'visible'})
+});
+
+$(document).ready(function(){
+    $('.testimonials').slick({
+        centerMode: true,
+        centerPadding: '100px',
+        autoplay: true,
+        autoplaySpeed: 5000,
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+        dots:true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    adaptiveHeight: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                },
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        adaptiveHeight: true,
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        centerMode: false,
+
+                    },
+                }
+        ]
+    });
+});
+
+$(document).ready(function(){
+    $('.blog').slick({
+        centerPadding: '100px',
+        autoplay: true,
+        autoplaySpeed: 5000,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        arrows: false,
+        dots:true,
+        responsive: [
+                        {
+                breakpoint: 1024,
+                settings: {
+                    adaptiveHeight: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                },
+                },
+                {
+                breakpoint: 600,
+                settings: {
+                    adaptiveHeight: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    centerMode: false,
+                },
+                }
+        ]
+    });
 });
 
       // cssEase: 'linear',
