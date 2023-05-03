e4d5a4cefd26a2b9f3537a428c76baa8156ccadb
xcixor <pndungu54@gmail.com>
2023-05-03 13:38:53 +0300

    chore(content): add contact us template

--- app/contact/presentation/views/index.py
+++ app/contact/presentation/views/index.py
@@ -1,6 +1,13 @@
+from typing import Any, Dict
 from django.views.generic import TemplateView
+from contact.forms import ContactEmailForm
 
 
 class ContactView(TemplateView):
 
     template_name = 'contact/index.html'
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context.update({'form': ContactEmailForm})
+        return context
