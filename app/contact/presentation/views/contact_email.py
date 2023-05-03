ec22666fa2051096ac56379cb65913e129deefef
xcixor <pndungu54@gmail.com>
2023-05-03 17:12:10 +0300

    feat(emails): send contact us email

--- app/contact/presentation/views/contact_email.py
+++ app/contact/presentation/views/contact_email.py
@@ -3,17 +3,17 @@ from django.views.generic import FormView
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.utils.translation import gettext_lazy as _
-from common.email import HtmlEmailMixin
 from contact.forms import ContactEmailForm
 
 
-class ContactEmailView(FormView, HtmlEmailMixin):
+class ContactEmailView(FormView):
 
     template_name = 'contact/index.html'
     form_class = ContactEmailForm
     success_url = reverse_lazy('contact:index')
 
     def form_valid(self, form):
+        form.send_email()
         success_message = _("Thank you for contacting us. We will be "
                             "getting back to you shortly!")
         messages.add_message(
