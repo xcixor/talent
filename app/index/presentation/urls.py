36f362350eb6b458bc5ac25c92e44c9fd34b438d
xcixor <pndungu54@gmail.com>
2023-05-04 17:23:48 +0300

    feat(index): add a page to display a service details

--- app/index/presentation/urls.py
+++ app/index/presentation/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from index.presentation.views import IndexView
+from index.presentation.views import IndexView, ServiceView
 
 app_name = 'index'
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
+    path('service/<slug:slug>/', ServiceView.as_view(), name='service'),
 ]
