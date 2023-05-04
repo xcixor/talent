6a8ab3f7d46a56fc475c934ae4b3e90a09de25cc
xcixor <pndungu54@gmail.com>
2023-05-04 13:15:30 +0300

    feat(index): add a means to update carousel dynamically

--- app/index/admin.py
+++ app/index/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from index.models import CarouselItem, Action
 
-# Register your models here.
+
+@admin.register(CarouselItem)
+class CarouselItemAdmin(admin.ModelAdmin):
+
+    list_display = ['html_stripped']
+
+
+admin.site.register(Action)
