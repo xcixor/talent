7a93ee39297a2e68d3c6ba86b2291003442a8eae
xcixor <pndungu54@gmail.com>
2023-05-04 13:15:30 +0300

    feat(index): add a means to update carousel dynamically

--- app/index/presentation/views/index.py
+++ app/index/presentation/views/index.py
@@ -1,6 +1,13 @@
+from typing import Any, Dict
 from django.views.generic import TemplateView
+from index.models import CarouselItem
 
 
 class IndexView(TemplateView):
 
     template_name = 'index/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({'carousel_items': CarouselItem.objects.all()})
+        return context
