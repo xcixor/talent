7ef9baa59b86976a73964ccb9eb176930a6d9320
xcixor <pndungu54@gmail.com>
2023-05-06 11:28:03 +0300

    chore(admin): hide slug fields which are autogenerated

--- app/job/admin.py
+++ app/job/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from job.models import Industry, JobListing
 
-admin.site.register(Industry)
-admin.site.register(JobListing)
+
+@admin.register(Industry)
+class IndustryAdmin(admin.ModelAdmin):
+    exclude = ['slug']
+
+
+@admin.register(JobListing)
+class JobListingAdmin(admin.ModelAdmin):
+    exclude = ['slug']
