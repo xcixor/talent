9893df309ffdc38b60593a5939270ac42c2022d6
xcixor <pndungu54@gmail.com>
2023-05-12 11:59:09 +0300

    chore(content): populate testimonials with db data

--- app/testimonials/presentation/views/index.py
+++ app/testimonials/presentation/views/index.py
@@ -1,6 +1,10 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from testimonials.models import Testimony
 
 
-class TestimonialsIndexView(TemplateView):
+class TestimonialsIndexView(ListView):
 
     template_name = 'testimonials/index.html'
+    model = Testimony
+    context_object_name = 'testimonies'
+    paginate_by = 5
