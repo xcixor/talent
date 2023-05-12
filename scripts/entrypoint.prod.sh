3de6a9e45af12aa43cd50df935213202aeb7c162
xcixor <pndungu54@gmail.com>
2023-05-12 10:59:47 +0300

    chore(data): add testimonials

--- scripts/entrypoint.prod.sh
+++ scripts/entrypoint.prod.sh
@@ -4,8 +4,10 @@ postgres-healthcheck
 python manage.py makemigrations
 python manage.py migrate
 python manage.py create_admin
+python manage.py create_mock_users
 python manage.py loaddata industries.json
 python manage.py loaddata carousel.json
 python manage.py loaddata about
+python manage.py loaddata testimonials
 gunicorn -b 0.0.0.0:${PORT} core.wsgi:application --timeout 90
 exec "$@"
