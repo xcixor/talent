43d9b12702dceb349307d83dd1198f077c8fbae9
xcixor <pndungu54@gmail.com>
2023-05-15 16:56:07 +0300

    feat(blog): add blog app

---
+++ app/blog/presentation/views/search.py
@@ -0,0 +1,24 @@
+from django.views import View
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+
+
+class BlogSearchView(View):
+
+    template_name = 'pes_admin/view_applications.html'
+
+    def get(self, request):
+        search_query = request.GET.get('blog_search')
+        if search_query:
+            self.request.session['blog_search'] = search_query
+        search_query = self.request.GET.get("blog_search")
+        return redirect(reverse_lazy('blog:posts'))
+
+
+class ClearBlogSearchView(View):
+
+    template_name = 'pes_admin/view_applications.html'
+
+    def get(self, request):
+        self.request.session.pop('blog_search', None)
+        return redirect(reverse_lazy('blog:posts'))
--- app/core/settings/base.py
+++ app/core/settings/base.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'about.apps.AboutConfig',
     'contact.apps.ContactConfig',
     'testimonials.apps.TestimonialsConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -85,6 +86,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'blog.context_processors.posts'
             ],
         },
     },
