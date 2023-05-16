c2dc64c3537eee859dc5dc31d31a8aa67eed1017
xcixor <pndungu54@gmail.com>
2023-05-16 16:54:34 +0300

    deployment(containers): pause fixture installation as db already populated

--- scripts/entrypoint.prod.sh
+++ scripts/entrypoint.prod.sh
@@ -3,12 +3,12 @@ python manage.py collectstatic --no-input
 postgres-healthcheck
 python manage.py makemigrations
 python manage.py migrate
-python manage.py create_admin
-python manage.py create_mock_users
-python manage.py loaddata industries.json
-python manage.py loaddata carousel.json
-python manage.py loaddata about
-python manage.py loaddata testimonials
-python manage.py loaddata posts
+# python manage.py create_admin
+# python manage.py create_mock_users
+# python manage.py loaddata industries.json
+# python manage.py loaddata carousel.json
+# python manage.py loaddata about
+# python manage.py loaddata testimonials
+# python manage.py loaddata posts
 gunicorn -b 0.0.0.0:${PORT} core.wsgi:application --timeout 90
 exec "$@"
