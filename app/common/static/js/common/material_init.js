aeefe32e304d85916e9f9dc31bc578e477466ece
xcixor <pndungu54@gmail.com>
2023-05-18 13:10:08 +0300

    style(links): color links

--- app/common/static/js/common/material_init.js
+++ app/common/static/js/common/material_init.js
@@ -1,24 +1,21 @@
-function subtractYears(date, years) {
-  date.setFullYear(date.getFullYear() - years);
-  return date;
-}
+$(document).ready(function () {
+	const subtractYears = function (date, years) {
+		date.setFullYear(date.getFullYear() - years);
+		return date;
+	};
+	const now = new Date();
+	const lastDatePossible = subtractYears(now, 60);
+	const minDateOfBirth = subtractYears(new Date(), 18);
+	console.log(lastDatePossible.getFullYear(), minDateOfBirth.getFullYear());
+	$(".datepicker").datepicker({
+		yearRange: [lastDatePossible.getFullYear(), minDateOfBirth.getFullYear()],
+		format: "mm/dd/yyyy",
+		setDefaultDate: true,
+	});
+	M.updateTextFields();
+	$(".sidenav").sidenav();
 
-$(document).ready(function(){
-  const now = new Date();
-  const lastDatePossible = subtractYears(now, 60);
-  const minDateOfBirth = subtractYears((new Date()), 18);
-  console.log(lastDatePossible.getFullYear(), minDateOfBirth.getFullYear())
-    $('.datepicker').datepicker({
-      yearRange:[lastDatePossible.getFullYear(), minDateOfBirth.getFullYear()],
-      format:'mm/dd/yyyy',
-      setDefaultDate:true
-    });
-  M.updateTextFields();
-  $('.sidenav').sidenav();
-
-
-  M.updateTextFields();
-  $('.sidenav').sidenav();
-  $('.modal').modal();
-  });
-
+	M.updateTextFields();
+	$(".sidenav").sidenav();
+	$(".modal").modal();
+});
