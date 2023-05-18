5e1106d1067d942e256728a14fe3bfac6df9273c
xcixor <pndungu54@gmail.com>
2023-05-18 14:19:41 +0300

    chore(registration): initialize date with a script in html file to fix occassional uninitialization

--- app/common/static/js/common/material_init.js
+++ app/common/static/js/common/material_init.js
@@ -7,14 +7,11 @@ $(document).ready(function () {
 	const lastDatePossible = subtractYears(now, 60);
 	const minDateOfBirth = subtractYears(new Date(), 18);
 	console.log(lastDatePossible.getFullYear(), minDateOfBirth.getFullYear());
-	$(".datepicker").datepicker({
-		yearRange: [lastDatePossible.getFullYear(), minDateOfBirth.getFullYear()],
-		format: "mm/dd/yyyy",
-		setDefaultDate: true,
-	});
-	M.updateTextFields();
-	$(".sidenav").sidenav();
-
+	// $(".datepicker").datepicker({
+	// 	yearRange: [lastDatePossible.getFullYear(), minDateOfBirth.getFullYear()],
+	// 	format: "mm/dd/yyyy",
+	// 	setDefaultDate: true,
+	// });
 	M.updateTextFields();
 	$(".sidenav").sidenav();
 	$(".modal").modal();
