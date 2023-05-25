7c3ad32490d5cce741b86a5e6b9a8dbcf9f22af4
xcixor <pndungu54@gmail.com>
2023-05-25 16:53:40 +0300

    chore(deployment): change server ip

--- scripts/entrypoint.prod.sh
+++ scripts/entrypoint.prod.sh
@@ -15,5 +15,5 @@ python manage.py create_admin
 # python manage.py loaddata posts
 python manage.py train
 echo 'Running server...'
-python manage.py runserver localhost:${PORT}
+python manage.py runserver 0.0.0.0:8500
 exec "$@"
