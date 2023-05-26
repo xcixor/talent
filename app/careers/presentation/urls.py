0858999d9d39add3c27942c11936a721c9cafb09
xcixor <pndungu54@gmail.com>
2023-05-26 15:34:12 +0300

    feat(careers): display a career details

--- app/careers/presentation/urls.py
+++ app/careers/presentation/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from careers.presentation.views import IndexView
+from careers.presentation.views import IndexView, CareerView
 
 
 app_name = 'careers'
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
+    path('<slug:slug>/', CareerView.as_view(), name='career'),
 ]
