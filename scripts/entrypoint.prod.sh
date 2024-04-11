29fbf6136516d8d094d5fa7aa298f889778f1ced
xcixor <pndungu54@gmail.com>
2024-04-11 10:49:04 +0300

    Load Initial Data
    Load initial data for industries, index, about, and testimonials using Django's `loaddata` management command. The data is loaded from JSON fixtures.
    
    This commit enables loading of initial data for key sections of the application during the production setup process.

--- scripts/entrypoint.prod.sh
+++ scripts/entrypoint.prod.sh
@@ -8,10 +8,10 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py create_admin
 # python manage.py create_mock_users
-# python manage.py loaddata industries.json
-# python manage.py loaddata index.json
-# python manage.py loaddata about
-# python manage.py loaddata testimonials
+python manage.py loaddata industries.json
+python manage.py loaddata index.json
+python manage.py loaddata about
+python manage.py loaddata testimonials
 # python manage.py loaddata posts
 python manage.py train
 echo 'Running server...'
